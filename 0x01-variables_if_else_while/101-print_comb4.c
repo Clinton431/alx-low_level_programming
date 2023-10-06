719328048a21de3eafbb645b7f645bc3bfb45813
clinton431 <clintonnyakoe@gmail.com>
2023-10-06 07:14:54 -0700

    changes

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -16,10 +16,9 @@ int main(void)
 			{
 				if (l > m && m > n)
 				{
-
 					putchar(n);
 					putchar(m);
-					purchar(l);
+					putchar(l);
 					if (n != 55 || m != 56)
 					{
 						putchar(',');
