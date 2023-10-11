0652a744acc95e942f07109e50a2ddbda2d76a87
clinton431 <clintonnyakoe@gmail.com>
2023-10-11 07:18:24 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,1 +1,16 @@
+#include "main.h"
 
+/**
+ * main -  test if a function that checks is number is
+ * positive or negative works then 0 is passed as the number
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
