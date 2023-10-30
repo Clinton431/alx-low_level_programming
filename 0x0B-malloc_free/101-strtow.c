01832f0facecee41da87de2c6ede099ef468e34b
clinton431 <clintonnyakoe@gmail.com>
2023-10-30 08:22:20 -0700

    changes

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -2,73 +2,69 @@
 #include "main.h"
 
 /**
- *  count_word - helper function to count the number of words in a string
- *  @s: string to evaluate
- *  Return: number of words
+ * count_words - estimates the number of words in strings
+ * @z: the string
+ * Return: the number of words
  */
-int count_word(char *s)
+int count_words(char *z)
 {
-	int flag = 0;
-	int c;
-	int w = 0;
+	int red, v, y;
 
-	for (c = 0; s[c] != '\0'; c++)
-	{
-		if (s[c] == ' ')
-			flag = 0;
-		else if (flag == 0)
+	red = 0;
+	y = 0;
+
+	for (v = 0; z[v] != '\0'; v++)
+
+		if (z[v] == ' ')
 		{
-			flag = 1;
-			w++;
+			red = 0;
 		}
-	}
-
-	return (w);
+			else if (red == 0)
+		{
+			red = 1;
+			y++;
+		}
+	return (y);
 }
-
 /**
- *   **strtow - splits a string into words
- *   @str: string to split
- *   Return: pointer to an array of strings (Success) or NULL (Error)
+ * strtow - function that splits a string into words
+ * @str: the string
+ * Return: pointer to an array of strings or Null
  */
 char **strtow(char *str)
 {
-	char **matrix, *tmp;
-	int i = 0;
-	int k = 0;
-	int len = 0;
-	int c =  0;
-	int start, end, words;
+	char **blue, *green;
+	int f, r = 0, p = 0, words, v = 0, a, z;
 
-	while (*(str + len))
-		len++;
-	words = count_word(str);
+	while (*(str + p))
+		p++;
+	words = count_words(str);
 	if (words == 0)
 		return (NULL);
-
-	for (i = 0; i <= len; i++)
+	blue = (char **)malloc(sizeof(char *) * (words + 1));
+	if (blue == NULL)
+		return (NULL);
+	for (f = 0; f < p; f++)
 	{
-		if (str[i] == ' ' || str[i] == '\0')
+		if (str[f] == ' ' || str[f] == '\0')
 		{
-			if (c)
+			if (v)
 			{
-				end = i;
-				tmp = (char *) malloc(sizeof(char) * (c + 1));
-				if (tmp == NULL)
+				z = f;
+				green = (char *)malloc(sizeof(char) * (v + 1));
+				if (green == NULL)
 					return (NULL);
-				while (start < end)
-					*tmp++ = str[start++];
-				*tmp = '\0';
-				matrix[k] = tmp - c;
-				k++;
-				c = 0;
+				while (a < z)
+					*green++ = str[a++];
+				*green = '\0';
+				blue[r] = green - v;
+				r++;
+				v = 0;
 			}
 		}
-		else if (c++ == 0)
-			start = i;
+		else if (v++ == 0)
+			a = f;
 	}
-
-	matrix[k] = NULL;
-
-	return (matrix);
+	blue[r] = NULL;
+	return (blue);
 }
