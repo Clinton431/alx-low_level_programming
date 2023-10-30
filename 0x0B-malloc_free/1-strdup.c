305118e57bb91a8bafcb9262b6a5e8794a345d91
clinton431 <clintonnyakoe@gmail.com>
2023-10-30 07:30:10 -0700

    strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include "main.h"
+
+
+/**
+ * _strdup - get ointer to new mem location
+ * @str: string to duplicate
+ * Return: pointer to the copied string (Success), NULL (Error)
+ */
+
+char *_strdup(char *str)
+{
+	char *dup;
+	unsigned int i = 0;
+	unsigned int len = 0;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[len])
+		len++;
+	dup = malloc(sizeof(char) * (len + 1));
+
+	if (dup == NULL)
+		return (NULL);
+	while ((dup[i] = str[i]) != '\0')
+		i++;
+	return (dup);
+}
+
