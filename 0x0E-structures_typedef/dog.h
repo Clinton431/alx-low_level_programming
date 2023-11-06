c854d039b76afdc0a7e3dba2ef2c473709713dc3
clinton431 <clintonnyakoe@gmail.com>
2023-11-06 03:22:44 -0800

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,1 +1,28 @@
+#ifndef DOG_H
+#define DOG_H
 
+/**
+ * struct dog - composite data type
+ * @name: name of the dog
+ * @age: a floating point number, dog's age
+ * @owner: a string of characters, dog's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+/**
+ * dog_t - the typedef
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *a);
+
+#endif
