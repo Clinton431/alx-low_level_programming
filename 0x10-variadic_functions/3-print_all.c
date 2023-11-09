e704700e96b72f26574968ac1b79e696a6f11083
clinton431 <clintonnyakoe@gmail.com>
2023-11-09 06:20:09 -0800

    prints all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,55 +1,50 @@
 #include "variadic_functions.h"
-	#include <stdarg.h>
-	#include <stdio.h>
+#include <stdio.h>
+#include <stdarg.h>
 
+/**
+ * print_all - function that prints anything
+ * @format: is a list of types of arguments passed to the function
+ * You are not allowed to use for, goto, ternary operator, else, do ... while
+ */
+void print_all(const char * const format, ...)
+{
+	int f = 0;
+	char *str, *con = "";
 
-	/**
-	 * print_all - prints anything
-	 * @format: list of types of arguments passed to the function
-	 */
-	void print_all(const char * const format, ...)
+	va_list prints;
+
+	va_start(prints, format);
+
+	if (format)
 	{
-		int i = 0;
-		char *str, *sep = "";
-
-
-		va_list list;
-
-
-		va_start(list, format);
-
-
-		if (format)
+		while (format[f])
 		{
-			while (format[i])
+			switch (format[f])
 			{
-				switch (format[i])
-				{
-					case 'c':
-						printf("%s%c", sep, va_arg(list, int));
-						break;
-					case 'i':
-						printf("%s%d", sep, va_arg(list, int));
-						break;
-					case 'f':
-						printf("%s%f", sep, va_arg(list, double));
-						break;
-					case 's':
-						str = va_arg(list, char *);
-						if (!str)
-							str = "(nil)";
-						printf("%s%s", sep, str);
-						break;
-					default:
-						i++;
-						continue;
-				}
-				sep = ", ";
-				i++;
+				case 'c':
+					printf("%s%c", con, va_arg(prints, int));
+					break;
+				case 'i':
+					printf("%s%d", con, va_arg(prints, int));
+					break;
+				case 'f':
+					printf("%s%f", con, va_arg(prints, double));
+					break;
+				case 's':
+					str = va_arg(prints, char *);
+					if (!str)
+						str = "(nil)";
+					printf("%s%s", con, str);
+					break;
+				default:
+					f++;
+					continue;
 			}
+			con = ", ";
+			f++;
 		}
-
-
-		printf("\n");
-		va_end(list);
 	}
+	printf("\n");
+	va_end(prints);
+}
