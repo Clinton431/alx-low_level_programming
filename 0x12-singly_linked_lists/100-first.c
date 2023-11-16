56b87d9203c581274625859ae09e8d84d13c2203
Clinton431 <clintonnyakoe@gmail.com>
2023-11-16 05:26:24 -0800

    task-100

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
+void first(void) __attribute__ ((constructor));
+
 /**
- * first - prints a sentence before the main function is executed
- *
- * Description: function that executes automatically before main and
- * prints the message to the console
+ *  first - prints a sentence before the main
+ *  function is executed
  */
-void first(void)__attribute__((constructor))
+void first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
