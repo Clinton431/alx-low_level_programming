a30a7505656aa55f577474c88b0b37544d638370
Clinton431 <clintonnyakoe@gmail.com>
2023-11-27 05:20:21 -0800

    fileIO

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,51 +1,32 @@
-#include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 /**
- *  read_textfile - fuction that read a text file and
- * print it out the POSIX stdout
- *  @filename: body of text to print.
- *  @letters: max char to print.
- *  Return: number of chars printed.
+ * read_textfile - function that reads a text file and prints
+ * it to the POSIX standard output
+ * @filename: text file
+ * @letters: number of letters
+ * Return: the actual number of letters it could read and print
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int rd = 0;
-	int fd = 0;
-	int err = 0;
 	char *buf;
-
-	if (!filename || !letters)
-		return (0);
+	ssize_t fd;
+	ssize_t f;
+	ssize_t k;
 
 	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	k = read(fd, buf, letters);
+	f = write(STDOUT_FILENO, buf, k);
 
-	if (fd < 0)
-		return (0);
-	buf = malloc(sizeof(char) * letters + 1);
-	if (!buf)
-		return (0);
-	rd = read(fd, buf, letters);
-
-	if (rd < 0)
-	{
-		free(buf);
-		return (0);
-	}
-	buf[letters] = '\0';
-	err = write(STDOUT_FILENO, buf, rd);
-
-	if (err <= 0)
-	{
-		free(buf);
-		return (0);
-	}
 	free(buf);
 	close(fd);
-	return (rd);
+	return (f);
 }
